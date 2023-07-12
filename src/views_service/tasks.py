11ee8b244d4e1bd40ace080a9a4d91ac6bc4abbf
0xshodan <hngdean@gmail.com>
2023-07-13 01:08:55 +0300

    x

--- src/views_service/tasks.py
+++ src/views_service/tasks.py
@@ -3,7 +3,7 @@ from src.views_service.views_manager import ViewsManager
 import asyncio
 from celery.utils.log import get_logger
 
-logger= get_logger(__name__)
+logger = get_logger(__name__)
 
 
 @celery_app.task
@@ -11,21 +11,26 @@ def view_channel(name: str, posts: list[int], task: dict):
     views_manager = ViewsManager()
     asyncio.run(views_manager.view_channel(name, task, posts))
 
+
 # @celery_app.task
 # def load_accounts():
 #     manager = manager_init.get_manager()
 #     asyncio.run(manager.add_accounts())
 
+
 async def aiiter(q):
     return [i async for i in q]
+
 
 @celery_app.task
 def check_new_posts():
     views_manager = ViewsManager()
+    print("task")
     channels = asyncio.run(views_manager.get_channels())
+    print(channels)
     for channel in channels:
         last_post = asyncio.run(views_manager.get_last_post_id(channel["name"]))
         if last_post > channel["last_post_id"]:
-            posts = [i for i in range(channel["last_post_id"]+1, last_post+1)]
+            posts = [i for i in range(channel["last_post_id"] + 1, last_post + 1)]
             asyncio.run(views_manager.change_last_post_id(channel["name"], last_post))
             view_channel.delay(channel["name"], posts, channel["task"])
